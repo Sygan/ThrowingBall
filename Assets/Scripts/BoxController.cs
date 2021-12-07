cb8410bff5b65beb63d28f462669a534e02d7f4a
Mateusz  Pusty <sygan@MacBook-Air-Mateusz.local>
2021-12-07 11:59:52 +0100

    Added Scriptable Objects

--- Assets/Scripts/BoxController.cs
+++ Assets/Scripts/BoxController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class BoxController : MonoBehaviour
 {
+    public Box BoxData;
+    
     private BallController currentBall;
     
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
-            GameManager.Instance.AddScore(1);
+            Debug.Log("Adding " + BoxData.Points + " points");
+            GameManager.Instance.AddScore(BoxData.Points);
             currentBall = other.GetComponent<BallController>();
             
             Invoke(nameof(ReturnBall), 1f);
